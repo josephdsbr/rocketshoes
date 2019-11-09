af1403723fd1c2ec95f00345a2da0c0c4afbafa5
josephdsbr <josephdsbr@gmail.com>
2019-11-09 19:30:57 -0300

    initial commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+import {MdShoppingBasket} from 'react-icons/md';
+import {connect} from 'react-redux';
+
+import {Container, Cart} from './styles';
+import Logo from '../../static/images/logo.svg';
+
+function Header({cartSize}) {
+  return (
+    <Container>
+      <Link to="/">
+        <img src={Logo} alt="Rocketshoes" />
+      </Link>
+      <Cart to="/cart">
+        <div>
+          <strong>Meu carrinho</strong>
+          <span>
+            {cartSize <= 0 ? `${cartSize} item` : `${cartSize} itens`}
+          </span>
+        </div>
+        <MdShoppingBasket size={36} color="#fff" />
+      </Cart>
+    </Container>
+  );
+}
+
+export default connect(state => ({
+  cartSize: state.cart.length,
+}))(Header);
